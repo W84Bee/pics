1ae06d993eca7cd6f95eb47919759c5f71dee5a4
Brandon Alexander <bjvonalexander@gmail.com>
2023-01-27 22:40:40 -0600

    Running the Search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
 function App() {
     const handleSubmit = (term) => {
-        console.log('Do a search with', term);
+        searchImages(term);
     }
     return (
         <div>
