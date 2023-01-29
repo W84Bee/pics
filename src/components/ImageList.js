1a2b249dca23145e5149a8160bbbf90cfb461ca3
Brandon Alexander <bjvonalexander@gmail.com>
2023-01-29 03:55:15 -0600

    81. A Touch of Styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
+import './ImageList.css';
 import ImageShow from "./ImageShow";
 
 function ImageList({ images }) {
     const renderImages = images.map((image) => {
         return (<ImageShow key={image.id} image={image} />);
     });
-    return <div>{renderImages}</div>
+    return <div className="image-list">{renderImages}</div>
 }
 
 export default ImageList;
